e7f0d9b5b1cc0a93d7733f0456446f265609e3bd
PeterGy <petigyorgy@gmail.com>
2021-10-24 17:12:47 +0200

    Organised code

--- trigPlotter.py
+++ trigPlotter.py
@@ -1,6 +1,3 @@
-xfile = 'Data/Oct20_50.txt'
-yfile = 'Data/Oct20_51.txt'
-
 from pylab import *
 from numpy import *
 #python has matrices y,x
@@ -8,8 +5,6 @@ from numpy import *
 
 def mirror(y):
   return list(flip(y[0:32]))+list(flip(y[32:64]))+list(flip(y[64:96]))+list(flip(y[96:128]))+list(flip(y[128:160]))+list(flip(y[160:192]))
-
-
 
 def load_data(file):
   data = open(file, "r") #loads file
@@ -40,23 +35,6 @@ def veto_ambiguous_events(data):
         processed_data.append(line)
   return processed_data
 
-
-
-datax = load_data(xfile)
-datax = process_data(datax)
-print("actual event entries in x     ",len(datax))
-datax = veto_ambiguous_events(datax)
-print("unambiguous event entries in x",len(datax))
-
-datay = load_data(yfile)
-datay = process_data(datay)
-print("actual event entries in y     ",len(datay))
-datay = veto_ambiguous_events(datay)
-print("unambiguous event entries in y",len(datay))
-#print(datax)
-
-
-
 def pair_up(datax,datay):
   dt=1e-7 #maximum time difference between pairs
   pairs=[]
@@ -83,9 +61,12 @@ def pair_up(datax,datay):
 
   print("Number of corellatabale event pairs",len(pairs))
   #print("Number of uncorellatabale events",thrown_event_count)
-  Matrix = [[0 for x in datax[0][1]] for x in datax[0][1]]
+  return pairs
 
-  for pair in pairs: 
+def create_plot_data(pairs):
+  Matrix = [[0 for x in datax[0][1]] for x in datax[0][1]]
+  if len(pairs)>100000: print("Processing "+str(len(pairs))+" pairs. This will take a while.")
+  for pair in pairs: #reorders channels so that they are actually in order. Takes a long time for large samples
     eventx=pair[0][1]
     eventx=list(eventx)
     eventx=mirror(eventx)
@@ -97,75 +78,78 @@ def pair_up(datax,datay):
   Matrix=[i[3*16:9*16] for i in Matrix[3*16:9*16]] #crops off blank channels
   return Matrix  
 
+def make_mystical_plot(Matrix):
+  #create plotting based on poorly understood and even more poorly explained matplotlib example
+  x=range(len(Matrix))
+  y=[sum(i) for i in Matrix]
+  y2=[sum(i) for i in array(Matrix).transpose()]
 
-Matrix = pair_up(datax,datay)
-
-matshow(Matrix,cmap=get_cmap("BuPu"))
-
-
-
-
-x=range(len(Matrix))
-y=[sum(i) for i in Matrix]
-bar(x,y,width=1)
-#show()
-
-MT=array(Matrix).transpose()
-y2=[sum(i) for i in MT]
-barh(x,y2,height=1)
-#show()
-
-def scatter_hist(x, y, ax, ax_histx, ax_histy,Matrix):
-    # no labels
-    ax_histx.tick_params(axis="x", labelbottom=False)
-    ax_histy.tick_params(axis="y", labelleft=False)
+  def scatter_hist(x, y, ax, ax_histx, ax_histy,Matrix):
+      # no labels
+      ax_histx.tick_params(axis="x", labelbottom=False)
+      ax_histy.tick_params(axis="y", labelleft=False)
 
 
 
-    ax.matshow(Matrix,cmap=get_cmap("BuPu"))
-    ax.tick_params(axis="x", labelbottom=True)
-    yticks([20,40,60,80,100])
+      ax.matshow(Matrix,cmap=get_cmap("BuPu"))
+      ax.tick_params(axis="x", labelbottom=True)
+      yticks([20,40,60,80,100])
 
-    ax_histx.bar(range(len(x)),x,width=1,color="darkorchid")
-    ax_histy.barh(range(len(y)),y,height=1,color="darkorchid")
+      ax_histx.bar(range(len(x)),x,width=1,color="darkorchid")
+      ax_histy.barh(range(len(y)),y,height=1,color="darkorchid")
+  
+  # definitions for the axes
+  left, width = 0.1, 0.65
+  bottom, height = 0.1, 0.65
+  spacing = 0.005
 
+  rect_scatter = [left, bottom, width, height]
+  rect_histx = [left, bottom + height + spacing, width, 0.2]
+  rect_histy = [left + width + spacing, bottom, 0.2, height]
 
+  # start with a square Figure
+  fig = figure(figsize=(8, 8))
 
+  ax = fig.add_axes(rect_scatter)
 
+  xlabel("Channel #")
+  ylabel("Channel #")
+  ax_histx = fig.add_axes(rect_histx, sharex=ax)
+  ylabel("Hits")
+  ax_histy = fig.add_axes(rect_histy, sharey=ax)
+  xlabel("Hits")
 
-
-# definitions for the axes
-left, width = 0.1, 0.65
-bottom, height = 0.1, 0.65
-spacing = 0.005
-
-
-rect_scatter = [left, bottom, width, height]
-rect_histx = [left, bottom + height + spacing, width, 0.2]
-rect_histy = [left + width + spacing, bottom, 0.2, height]
-
-# start with a square Figure
-fig = figure(figsize=(8, 8))
-
-ax = fig.add_axes(rect_scatter)
-
-xlabel("Channel #")
-ylabel("Channel #")
-
-ax_histx = fig.add_axes(rect_histx, sharex=ax)
-ylabel("Hits")
-
-ax_histy = fig.add_axes(rect_histy, sharey=ax)
-xlabel("Hits")
-
-
-
-scatter_hist(y, y2, ax, ax_histx, ax_histy, Matrix)
+  scatter_hist(y, y2, ax, ax_histx, ax_histy, Matrix)
+  savefig("Plots/plot.png",dpi=300)
 
 
 
 
-#
+def main(options): 
+  xfile = 'Data/'+options.x
+  yfile = 'Data/'+options.y
 
-savefig("Plots/plot.png",dpi=300)
-#show()
+  datax = load_data(xfile)
+  datax = process_data(datax)
+  print("actual event entries in x     ",len(datax))
+  datax = veto_ambiguous_events(datax)
+  print("unambiguous event entries in x",len(datax))
+
+  datay = load_data(yfile)
+  datay = process_data(datay)
+  print("actual event entries in y     ",len(datay))
+  datay = veto_ambiguous_events(datay)
+  print("unambiguous event entries in y",len(datay))
+
+
+  pairs = pair_up(datax,datay)
+  Matrix = create_plot_data(datax,datay)
+  make_mystical_plot(Matrix)
+
+
+if __name__=="__main__":
+  parser = OptionParser()	
+  parser.add_option('-x','--xfibers', dest='x', default = "Oct20_50.txt" ,help='The name of the txt file that has the x fibers')
+  parser.add_option('-y','--yfibers', dest='y', default = "Oct20_51.txt" ,help='The name of the txt file that has the y fibers')
+  options = parser.parse_args()[0]
+  main(options)
